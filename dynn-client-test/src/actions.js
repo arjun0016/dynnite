8dfbd9d082822d204a72d90734fb9eb112dc1ff3
Arjun P <arjunpknr0016@gmail.com>
2024-03-23 11:08:52 +0530

    action shawaya

--- dynn-client-test/src/actions.js
+++ dynn-client-test/src/actions.js
@@ -45,7 +45,7 @@ export const listCategories = async (dispatch) => {
 export const listProducts = async (dispatch, categoryName = '') => {
   dispatch({ type: PRODUCT_LIST_REQUEST });
   try {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASEURL}/api/products?category=${categoryName}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASEURL}/api/products?category=Shawaya`);
     return dispatch({
       type: PRODUCT_LIST_SUCCESS,
       payload: data,
